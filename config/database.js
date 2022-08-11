e362a7d69fe3412a99d27922063ec6af878a1823
hovhannes.babayan <hovhannes.babayan@fouraitch.com>
2022-08-11 17:55:44 +0400

    add healthcheck and change database config to url

--- config/database.js
+++ config/database.js
@@ -4,12 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'upload-service'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        uri: env('DATABASE_URL', 'mongodb://127.0.0.1:27017/upload-service'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),

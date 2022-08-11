fc4d234bc80ae0e615ffb7072813a4a09918dc6f
hovhannes.babayan <hovhannes.babayan@fouraitch.com>
2022-08-11 20:13:44 +0400

    add env variables

--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('API_URL'),
+  production: true,
   admin: {
+    autoOpen: false,
+    url: env('ADMIN_URL'),
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'f88183ae816e53e9602872f2b0632a63'),
     },
